e5a4ecc14355b02b871bd3ec98b7b75237c28d4f
maru <childerolando@look.com>
2021-11-22 01:25:17 +0100

    框架

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Counter
+
+
+class itemPairs:
+  """ base class for itemPairs """
+  counterDict = {}
+  manhattanDistance = []
+  level = 0
+
+  def __init__(self, level) -> None:
+    self.level = level
+    pass
+  
+  def count_disk(self, path):
+    pass
+
+  def generate_next_level_pairs(self):
+    pass
+
+  def reduce_dimension(self):
+    pass
+
+  def prune_non_frequent(self):
+    pass
+
+  def manhattan_distance(self, seta, setb):
+    return len(seta|setb)-len(seta&setb)
+
+  def generate_manhattan_distance_matrix(self, seta, setb):
+    return len(seta|setb)-len(seta&setb)
+
+
+if __name__ == "__main__":
+  f = frozenset([1,2])
+  s = set([1,2,3,4])
+  print(f.issubset(s))
+  
